e509a4499310e130504498c954bc6f1f3c980570
Mike Mwanje <mwanjemike767@gmail.com>
2020-10-03 22:03:46 +0300

    factorialTest Including the infinity test as well

--- factorial.js
+++ factorial.js
@@ -2,22 +2,22 @@
  * n!, is the product of all positive integers less than or equal to n
  * Given a number n , n! = n.(n-1).(n-2).(n-3)......1
  */
-const calculator = require('./calculator');
+const calculator = require("./calculator");
 
 function factorial(number) {
   let result = 1;
   if (number < 0) {
-    result = 'Number can\'t be a negative';
+    result = "Number can't be a negative";
     return result;
   } else if (number === 0 && number === 1) {
     result *= 1;
     return result;
   } else {
-    for(let counter = number; counter > 1; counter -= 1) {
+    for (let counter = number; counter > 1; counter -= 1) {
       result *= counter;
-    }  
-    return result; 
-  } 
+    }
+    return result;
+  }
 }
 
 // calculator(30, factorial);
--- test/factorialTest.js
+++ test/factorialTest.js
@@ -1,31 +1,42 @@
-const assert = require('chai').assert;
+/* eslint-disable no-undef */
+const { assert } = require('chai');
 const F = require('../factorial');
 
-describe('factiorial numbers', function(){
-  let factorial = F;
+describe('Factorials of numbers', () => {
+  const factorial = F;
 
-//   Checks if number < 0 returns a string
-  it('fact(-5) is String', function(){ 
-    assert.isString(factorial(-5)); 
-  })
+  //   Checks if number < 0 returns a string
+  it('should return a message for negative numbers', () => {
+    assert.isString(factorial(-5));
+  });
 
-//   Checks if number = 0 returns a number
-  it('fact(0) is Number', function(){ 
-    assert.isNumber(factorial(0)); 
-  })
+  //   Checks if number = 0 returns a number
+  it('should return a number for factorial of 0', () => {
+    assert.isNumber(factorial(0));
+  });
 
-//   checks if number = 1 returns a number
-  it('fact(1) is Number', function(){ 
-    assert.isNumber(factorial(1)); 
-  })
+  //   checks if number = 1 returns a number
+  it('should return a number for factorial of 1', () => {
+    assert.isNumber(factorial(1));
+  });
 
-//   checks if the value of factorial(5) is equal 120
-  it('fact(5) = 120', function(){ 
+  //   checks if the value of factorial(5) is equal to 1
+  it('should return a number for factorial of 0', () => {
+    assert.equal(factorial(0), 1);
+  });
+
+  //   checks if the value of factorial(5) is equal to 1
+  it('should return a number for factorial of 1', () => {
+    assert.equal(factorial(1), 1);
+  });
+
+  //   checks if the value of factorial(5) is equal to 120
+  it('should return value 120 for factorial of 5', () => {
     assert.equal(factorial(5), 120);
-  })
+  });
 
-  //   checks if the value of factorial(5) is equal 120
-  it('fact(5) = 120', function(){ 
-    assert.isString(factorial(175), 1504960400);
-  })
-})
+  //   checks if the value of factorial(5) is equal to 120
+  it('should return infinity for factorial of infinity', () => {
+    assert.equal(factorial(175), 1504960400);
+  });
+});
