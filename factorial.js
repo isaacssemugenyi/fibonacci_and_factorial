2f82e532ec50c83ed5b64fdbc9b1386a91fa5f9b
Mike Mwanje <mwanjemike767@gmail.com>
2020-10-03 21:26:56 +0300

    Group members

--- factorial.js
+++ factorial.js
@@ -1,10 +1,4 @@
 /**
- * Group Members
- * Walter Ruganzu
- * Mike Mwanje
- * Isaac Ssemugenyi
- * Davies Wabuluka
- * 
  * n!, is the product of all positive integers less than or equal to n
  * Given a number n , n! = n.(n-1).(n-2).(n-3)......1
  */
